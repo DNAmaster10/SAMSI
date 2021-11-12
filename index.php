c4591b6052dcac49f333f3e72f7d22ca853e904e
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-12 09:12:26 +0000

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <title>Samsi</title>
+    <body>
+        <p>Welcome to Samsi</p>
+        <form action="/pages/login.php">
+            <input type="button" onclick="location.href='/pages/login.php';" value="Go to login page" />
+        </form>
+    </body>
+</html>
