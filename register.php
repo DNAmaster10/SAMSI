673c64772a42e2b26d6cd68398d64137453e3f49
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-12 23:57:19 +0000

    Update register.php

--- register.php
+++ register.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
   <body>
-    <form action="schoolRegComplete.php" method="post">
-      School Name: <input type="text" name="schoolName"><br>
-      Admin Account Name: <input type="text" name="adminName"><br>
-      Admin Account Password: <input type="text" name="adminPassword"><br>
+    <form action="schoolRegComplete.php" method="POST">
+      <p>School Name:</p> <input type="text" name="schoolName"><br>
+      <p>Admin Account Name:</p> <input type="text" name="adminName"><br>
+      <p>Admin Account Password:</p> <input type="text" name="adminPassword"><br>
       <input type="submit">
     </form>
   </body>
