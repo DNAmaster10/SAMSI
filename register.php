87d67dbfafc26ed248b2df71411e50b87f244b17
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-12 23:21:53 +0000

    Update register.php

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
       School Name: <input type="text" name="schoolName"><br>
       Admin Account Name: <input type="text" name="adminName"><br>
       Admin Account Password: <input type="text" name="adminPassword"><br>
-      <input type="submit">
+      <input type="submit"></input>
     </form>
   </body>
 </html>
