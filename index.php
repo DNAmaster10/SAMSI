a3dee8338bae64fc225bff7d62f22e1e528edc29
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-12 23:35:42 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <body>
         <p>Welcome to Samsi</p>
         <form action="/Pages/login.php">
-            <input type="button" onclick="location.href='/pages/login.php';" value="Go to login page" />
+            <input type="button" onclick="location.href='/Pages/login.php';" value="Go to login page" />
         </form>
         <form action="/Pages/register.php">
-            <input type="button" onclick="location.href='/pages/register.php';" value="Register a school" />
+            <input type="button" onclick="location.href='/Pages/register.php';" value="Register a school" />
         </form>
     </body>
 </html>
