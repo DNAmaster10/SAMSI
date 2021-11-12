338cd3347693ec810563c9a1c743949fc290ecc6
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-12 11:25:12 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,8 @@
         <form action="/pages/login.php">
             <input type="button" onclick="location.href='/pages/login.php';" value="Go to login page" />
         </form>
+        <form action="/pages/register.php">
+            <input type="button" onclick="location.href='/pages/register.php';" value="Register a school" />
+        </form>
     </body>
 </html>
