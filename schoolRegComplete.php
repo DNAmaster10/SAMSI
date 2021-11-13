6dc84cf4360f59d473a49d25a0a9e208ec71e970
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-13 01:04:57 +0000

    Update schoolRegComplete.php

--- schoolRegComplete.php
+++ schoolRegComplete.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html>
   <?php
+  $continue = (1);
   $schoolName = $_POST["schoolName"];
   $adminName = $_POST["adminName"];
   $adminPassword = $_POST["adminPassword];
   
+  //need to finish string validation
+  
   $schoolName = str_replace(" ", "");
   $schoolNameLength = strlen($schooName);
-  
   if ($schoolNameLength > (50)) {
-    $textOutput = "Your school name is too long"
-    $continue = (0)
+    $textOutput = ("Your school name is too long");
+    $continue = (0);
+    }
+    
+  if ($continue == (1)) {
+    if ( !file_exists(/Data/$schoolName) && !is_dir(/Data/$schoolName)) {
+      mkdir(/Data/$schoolName);
+      $textOutput = ("School successfully registered")
+    }
+    else {
+    $textOutput = ("Your school is already registered!")
   }
   ?>
   <body>
