c8a27d0f5f21fadd4a89709484db5b3524b22750
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-13 00:43:52 +0000

    Update schoolRegComplete.php

--- schoolRegComplete.php
+++ schoolRegComplete.php
@@ -1,6 +1,26 @@
 <!DOCTYPE html>
 <html>
+  <?php
+  $schoolName = $_POST["schoolName"];
+  $adminName = $_POST["adminName"];
+  $adminPassword = $_POST["adminPassword]
+  
+  $conn = new mysqli($schoolName, $adminName, $adminPassword);
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+  
+  $sql = "CREATE DATABASE IF NOT EXISTS $schooName;
+  if ($conn->query($sql) == TRUE) {
+    echo "Database for $schooName successfully created";
+  } else {
+    echo "Error creating database: " . $conn->error;
+  }
+                          
+  $conn->close();
+  ?>
   <body>
-    <p><?php echo $_POST["schoolName"]; ?> successfully registered!</p>
+    <p><?php echo $textOutput; ?></p>
   </body>
 </html>
