220e6ddf9a9c3069df113b64d887a9aa441f4a07
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-13 19:38:32 +0000

    Update schoolRegComplete.php

--- schoolRegComplete.php
+++ schoolRegComplete.php
@@ -6,13 +6,26 @@
   $adminName = $_POST["adminName"];
   $adminPassword = $_POST["adminPassword"];
   $textOutput = ('null');
-  
-  //need to finish string validation
+
   $schoolNameLength = strlen($schoolName);
-    
+  $schoolName = str_replace(" ","",$schoolName);
+  $schoolName = strtolower($schoolName);
+  
+  $adminNameLength = strlen($adminName);
+  $adminName = str_replace(" ","",$adminName);
+  $adminName = strtolower($adminName);
+  
+  if ($schoolNameLenght > 50) or (adminNameLength > 50) {
+  $continue = (0);
+  }
+  
   if ($continue == (1)) {
     if (!is_dir('/var/www/html/Data/'.($schoolName))) {
       mkdir('/var/www/html/Data/'.($schoolName));
+      mkdir("/var/www/html/Data/".($schoolName)."/accounts/admin");
+      $admin_txt_file = fopen($adminName, "w")
+      fwrite($admin_txt_file, $adminPassword."\n")
+      fclose($admin_txt_file)
       $textOutput = 'School successfully registered';
     }
     else {
