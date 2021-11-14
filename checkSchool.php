648d2912388a82b25a8b26daf64790f1cb74a877
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-14 02:20:28 +0000

    Update checkSchool.php

--- checkSchool.php
+++ checkSchool.php
@@ -1,9 +1,11 @@
 <?php
 $user_input = $_REQUEST["user_input"];
-if (!is_dir('/var/www/html/Data/'.$user_input)) {
+$time = time();
+if (is_dir('/var/www/html/Data/'.$user_input)) {
 echo "School exists!";
 }
 else {
-echo  "School does not exist!"; 
+echo  "School does not exist!";
 }
+echo $time;
 ?>
