7aa219b65cee1e747964f55494e3b05fac39ce0d
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-14 02:33:07 +0000

    Update checkSchool.php

--- checkSchool.php
+++ checkSchool.php
@@ -1,6 +1,7 @@
 <?php
 $user_input = $_REQUEST["user_input"];
 $time = time();
+$file_state = (file_exists('/var/www/html/Data/'.$user_input));
 if (file_exists('/var/www/html/Data/'.$user_input)) {
 echo "School exists!";
 }
@@ -8,4 +9,6 @@ else {
 echo  "School does not exist!";
 }
 echo $time;
+echo "<br>"
+echo $file_state
 ?>
