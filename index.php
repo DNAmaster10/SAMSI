ed721367f456f3da908e84b2d6076b5075b11cc3
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-11-26 09:41:51 +0000

    Update index.php

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
         <form action="/Pages/register.php">
             <input type="button" onclick="location.href='/Pages/register.php';" value="Register a school" />
         </form>
+        <form action="/Pages/test.php">
+            <input type="button" onclick="location.href='/Pages/test.php';" value="Register a school" />
+        </form>
     </body>
 </html>
