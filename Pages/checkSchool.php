c418f184f2d32a58b92467ccf75179d3a4735a7d
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-08 10:32:40 +0000

    Update checkSchool.php
    Testing

--- Pages/checkSchool.php
+++ Pages/checkSchool.php
@@ -1,11 +1,4 @@
 <?php
 $user_input = $_REQUEST["schoolNamePostContainer"];
-$file_name = "/var/www/html/Data/".$user_input;
-if (file_exists($file_name)) {
-  echo ("School exists!");
-  }
-else {
-  echo ("School does not exist!");
-}
 echo $user_input;
 ?>
