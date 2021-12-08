67da9e3eaf8c288ba28b3839e8d0fbeed4c00489
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-08 10:24:49 +0000

    Update re-install.bash

--- re-install.bash
+++ re-install.bash
@@ -1,8 +1,22 @@
-echo "Deleting and redownloading"
-rm -rfd /var/www/html/*
-wget -P https://raw.githubusercontent.com/DNAmaster10/SAMSI/main/installer.sh /var/www/html/installer.sh
-sh /var/www/html/installer.sh
-sudo chown -R www-data:www-data /var/www/html
-sudo chmod -R 770 /var/www/html
-echo "Finished installing"
+i=2;
+cd /var/www/html
+sudo git clone https://github.com/DNAmaster10/SAMSI.git
+while [ $i -le 2 ]
+do
+    sudo mkdir /var/www/temp
+    cd /var/www/temp
+    sudo git clone https://github.com/DNAmaster10/SAMSI.git
+    echo "Deleting and redownloading"
+    sudo rm -r /var/www/html
+    sudo mkdir /var/www/html
+    sudo chown www-data:www-data /var/www/html
+    sudo chmod 770 /var/www/html
+    echo "Finished installing"
+    sudo cp -r /var/www/temp/SAMSI /var/www/html
+    echo "Number: $i"
+    sudo rm -r -r /var/www/temp
+    echo -n "Press enter to update site"
+    read result
+done
+
 exit 3
