9d82450f7be2b50ee34b7bd7736a3f70b475a666
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-10 14:18:29 +0000

    Update request_message.php

--- Pages/request_message.php
+++ Pages/request_message.php
@@ -1,5 +1,5 @@
 <?php
-$message_file = fopen ("message_file","r");
+$message_file = fopen ("message_file.txt","r");
 echo json_encode(fread($message_file("webdictionary.txt"));
 fclose($message_file);
 ?>
