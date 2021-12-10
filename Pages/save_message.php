5c27f3659c6c80967042e08d52c0f3c1834e1341
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-10 14:21:52 +0000

    Update save_message.php

--- Pages/save_message.php
+++ Pages/save_message.php
@@ -1,5 +1,5 @@
 <?php
 $user_input=$_POST['sent_message'];
-$message_file = fopen("message_file.txt", "w");
+$message_file = fopen(getcwd()."message_file.txt", "w");
 fwrite($message_file, $user_input);
 ?>
