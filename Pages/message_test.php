d799b4cc219181239031911954d88250f43307f1
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-10 14:36:11 +0000

    Update message_test.php

--- Pages/message_test.php
+++ Pages/message_test.php
@@ -3,7 +3,7 @@
   <title>Messenger</title>
   <body>
     <p>Enter your message in the box bellow</p>
-    <input type="text" id="input_message" name="message_entry" onkeyup="send_mesage()">
+    <input type="text" id="input_message" name="message_entry" onkeyup="send_message()">
     <br>
     <p id="message_output_field">Current message: <?php $message_file = fopen("messages.txt","r") or die ("Unable to open message file!");
       echo fgets($message_file);
