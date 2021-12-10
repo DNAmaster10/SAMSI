531db533c4aa392701fdd3d3e0219946def49b35
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-10 10:27:54 +0000

    Create message_test.php

---
+++ Pages/message_test.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <title>Messenger</title>
+  <body>
+    <p>Enter your message in the box bellow</p>
+    <input type="text" id="input_message" name="message_entry" onkeyup="send_mesage()">
+    <br>
+    <p id="message_output_field">Current message: <?php $message_file = fopen("messages.txt","r") or die ("Unable to open message file!");
+      echo fgets($message_file);
+      fclose ($message_file);
+      
+      ?> </p>
+  </body>
+  <script>
+    var result = ("");
+    function send_message() {
+    console.log("Sending message to server");
+    $.ajax({
+      url: 'save_message.php',
+      type: 'POST',
+      dataType:'json',
+      data: {'sent_message':document.getElementById("input_message").value},
+      success: function(data) {
+      console.log("Message successfully sent!");
+    }
+    });
+    }
+    
+    function request_messages() {
+    4.ajax({
+    url: "request_message.php",
+    type: "POST",
+    dataType:"json",
+    success: function(recieved_message) {
+    console.log("Message recieved successfully!");
+    document.getElementById("message_output_field").innerHTML = ("Current message: " + recieved_message;
+    }
+    });
+    }
+    var i = 1;                  //  set your counter to 1
+
+    function myLoop() {         //  create a loop function
+      setTimeout(function() {   //  call a 3s setTimeout when the loop is called
+        console.log('Fetching messages');   //  your code here
+        request_messages()
+        i++;                    //  increment the counter
+        if (i > 0) {           //  if the counter < 10, call the loop function
+          myLoop();             //  ..  again which will trigger another 
+        }                       //  ..  setTimeout()
+     }, 1000)
+    }
+
+    myLoop();                   //  start the loop
+  </script>
+</html>
