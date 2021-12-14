f4cd810eb0bb7c6ca5c15eac93a849c8506408e2
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-14 11:36:41 +0000

    Update message_test.php

--- Pages/message_test.php
+++ Pages/message_test.php
@@ -16,43 +16,4 @@
       ?> </p>
   </body>
   <script src="script src="/Scripts/messenger.js"></script>
-  <script>
-       function send_message() {
-   console.log("Sending message to server");
-    $.ajax({
-      url: 'save_message.php',
-      type: 'POST',
-      dataType:'json',
-      data: {'sent_message':document.getElementById("input_message").value},
-      success: function(data) {
-      console.log("Message successfully sent!");
-    }
-    });
-    } 
-    var function request_messages() {
-    $.ajax({
-      url: "request_message.php",
-      type: "POST",
-      dataType:"json",
-      success: function(recieved_message) {
-      console.log("Message recieved successfully!");
-      document.getElementById("message_output_field").innerHTML = ("Current message: " + recieved_message);
-      }
-      });
-    }
-    var i = 1;                  
-
-    function myLoop() {       
-      setTimeout(function() {  
-        console.log('Fetching messages');  
-        request_messages()
-        i++;                
-        if (i > 0) {         
-          myLoop();          
-        }                      
-     }, 1000)
-    }
-
-    myLoop();                
-  </script>
 </html>
