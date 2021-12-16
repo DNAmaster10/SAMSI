a4668156c379e4f729ed45f5f12995c9fa2d7a0c
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-16 15:02:48 +0000

    Update login.php

--- Pages/login.php
+++ Pages/login.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
 <html>
   <title>Login</title>
   <body>
@@ -8,22 +7,7 @@
     <input type="text" id="school_text" name="school_txt" onkeyup="check_school()">
     <p id="output_field">School does not exist</p>
     
-    <script>
-    var result = ("");
-    function check_school() {
-    console.log("Checking if school exists");
-    $.ajax({
-      url: 'checkSchool.php',
-      type: 'POST',
-      dataType:'json',
-      data: {'post_var':document.getElementById("school_text").value},
-      success: function(data) {
-      console.log(data);
-      result = data;
-      document.getElementById("output_field").innerHTML = (result);
-    }
-    });
-    }
-    </script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="/Scripts/messenger.js"></script>
   </body>
 </html>
