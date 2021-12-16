defe583836e061c0f7874a8049bdc86d630759f5
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-16 16:09:55 +0000

    Update request_message.php

--- Pages/request_message.php
+++ Pages/request_message.php
@@ -1,5 +1,5 @@
 <?php
 $message_file = fopen (getcwd()."message_file.txt","r");
-echo json_encode(fread($message_file("message_file.txt"));
+echo json_encode(fread($message_file("message_file.txt")));
 fclose($message_file);
 ?>
