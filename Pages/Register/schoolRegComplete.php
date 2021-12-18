5f66fdf6182371ea8e29ab7cba6863c2b89491b3
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2021-12-18 01:28:17 +0000

    Update schoolRegComplete.php

--- Pages/Register/schoolRegComplete.php
+++ Pages/Register/schoolRegComplete.php
@@ -21,43 +21,20 @@
   $textOutput = ("An Error had occured");
   
   if ($schoolNameLength > 50) {
-  $continue = (0);
+    $continue = (0);
   }
   if ($adminNameLength > 50) {
-  $continue = (1);
+    $continue = (0);
   }
   
-  if ($continue == (1)) {
-    if (!is_dir($root.'/Data/'.$schoolName)) {
-      
-      $textOutput = ("An error occured setting up either your school, or your admin account. Contact a network administrator for more information on the matter");
-      mkdir($root."/Data");
-      mkdir($root."/Data/".$schoolName);
-      mkdir($root."/Data/".$schoolName."/Accounts");
-      mkdir($root."/Data/".$schoolName."/Accounts/Admin");
-      mkdir($root."/Data/".$schoolName."/Accounts/Admin/".$adminName);
-        
-      copy ($root."/Templates/adminPanelTemplate.txt",$root."Data/".($schoolName)."/Accounts/Admin/".$adminName."/".$adminPassword."Panel.txt");
-      
-      $path_to_file = $root."/Data/".($schoolName)."/Accounts/Admin/".$adminName."/".$adminPassword."Panel.txt";
-      $file_contents = file_get_contents($path_to_file);
-      $file_contents = str_replace("ADMIN_NAME_PLACEHOLDER",$adminName,$file_contents);
-      file_put_contents($path_to_file,$file_contents);
-
-      copy ($root."/Templates/logged_in.txt",$root."/Data/".$schoolName."/Accounts/Admin/".$adminName."/logged_in.txt");
-      
-      $file_contents_2 = file_get_contents($root."/Data/".$schoolName."/Accounts/Admin/".$adminName."/logged_in.txt");
-      $file_contents_2 = str_replace("false","true",$file_contents);
-      file_put_contents ($root."/Data/".$schoolName."/Accounts/Admin/".$adminName."/logged_in.txt",$file_contents_2);
-   
-      rename($root."/Data/".$schoolName."/Accounts/Admin/".$adminName."/".$adminPassword."Panel.txt",$root."/Data/".$schoolName."/Accounts/Admin/".$adminName."/".$adminPassword."Panel.php");
-      $admin_panel_path = "../Data/".$schoolName."/Accounts/Admin/".$adminName."/".$adminPassword."Panel.php";
-      $textOutput = 'School successfully registered';      
-    }
-    else {
-    $textOutput = 'Your school is already registered!';
+  if ($continue == 1) {
+    
   }
-  } ?>
+  
+  else {
+    $textOutput = ("An error occured. For more information, contact a network administrator");
+  }
+  ?>
   <body>
     <p> <?php echo $textOutput; ?> </p>
     <form action="<?php echo $admin_panel_path; ?>">
