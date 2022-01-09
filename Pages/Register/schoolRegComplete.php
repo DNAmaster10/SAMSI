9db90a59c629e881d89ff8c6306c6e6648ef6908
DNAmaster10 <louishollobon@outlook.com>
2022-01-09 17:12:46 +0000

    Stopped duplicate admin usernames

--- Pages/Register/schoolRegComplete.php
+++ Pages/Register/schoolRegComplete.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html>
   <?php
+  #connect to database
   include "/var/www/html/Includes/dbh.php";
   
-  
+  #get inputs from browser
   $continue = (1);
   $schoolName = $_POST["schoolName"];
   $adminName = $_POST["adminName"];
   $adminPassword = $_POST["adminPassword"];
   $textOutput = ("An Error has occured with the variable handling of the $schoolName variable");
-
+  
+  #String handling
   $schoolNameLength = strlen($schoolName);
   $schoolName = str_replace(" ","",$schoolName);
   $schoolName = strtolower($schoolName);
@@ -22,16 +24,23 @@
   
   $textOutput = ("An Error had occured");
   
-  if ($schoolNameLength > 50) {
+  if ($schoolNameLength > 25) {
     $continue = (0);
+	$textOutput = ("The entered school name is too long! Maximum of 25 characters allowed.");
   }
-  if ($adminNameLength > 50) {
+  if ($adminNameLength > 25) {
     $continue = (0);
+	$textOutput = ("The admin username length is too long! Maximum of 25 characters allowed.");
   }
-  
+  #add username and password to sql database
   if ($continue == 1) {
 	$sql = ("INSERT INTO admin_users (username, password) VALUES ('".$adminName."','".$adminPassword."');");
-	mysqli_query ($conn, $sql) or die(mysqli_error($mysqli));;
+	if (!(mysqli_query ($conn, $sql) or die(mysqli_error($mysqli)))) {
+		$textOutput = ("Username already in use. Please use a different one.");
+	}
+	else {
+	$textOutput = ("All done! You should now be able to login with your admin account to begin your SAMSi journey.");
+	}
   }
   
   else {
@@ -39,7 +48,7 @@
   }
   ?>
   <body>
-    <p> Done! </p>
+    <p> <?php echo $textOutput; ?></p>
     <form action="index.php">
       <input type="button" onclick="location.href='index.php';" value="Admin Panel" />
     </form>
