6de82aab8d30c25c2811e2aeab9b94ae2c2781ce
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-01-11 10:29:23 +0000

    Add files via upload

---
+++ Pages/login/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <title>Login</title>
+  <body>
+    <p>Enter your login credidentials bellow</p>
+    <br>
+    <form action="/Pages/Login/Login_submit.php">
+        <p>Username: </p><input type="text" id="username_text_box" name="username"><br>
+        <p>Password: </p><input type="text" id="password_text_box" name="password"><br>
+        <input type="submit" text="Login">
+    </form>
+  </body>
+</html>
+
