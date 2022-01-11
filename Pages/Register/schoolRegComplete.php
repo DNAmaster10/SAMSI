e0c6914b9bf31402108c5d2cdba55066274b0048
DNAmaster10 <louishollobon@outlook.com>
2022-01-11 20:27:12 +0000

    Added to SQL

--- Pages/Register/schoolRegComplete.php
+++ Pages/Register/schoolRegComplete.php
@@ -43,7 +43,7 @@
   #add username and password to sql database
   try {
   if ($continue == 1) {
-	$sql = ("INSERT INTO users (username, password, create_account_permission) VALUES ('".$adminName."','".$adminPassword."','yes');");
+	$sql = ("INSERT INTO users (username, password, create_account_permission, 'isAdmin) VALUES ('".$adminName."','".$adminPassword."','yes','yes');");
 	mysqli_query ($conn, $sql); 	
 	$textOutput = ("All done! You should now be able to login with your admin account to begin your SAMSi journey.");
 	}
