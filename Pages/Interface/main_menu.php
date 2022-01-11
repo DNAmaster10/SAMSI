63130c967c380a33efc8a0293b4c1d8a55c960db
DNAmaster10 <louishollobon@outlook.com>
2022-01-11 22:11:23 +0000

    e

--- Pages/Interface/main_menu.php
+++ Pages/Interface/main_menu.php
@@ -8,5 +8,8 @@ include "/Includes/check_user_pass.php";
 	</head>
 	<body>
 		<h1>Welcome back, <?php echo ($_SESSION["username"]."."); ?> </h1>
+		<?php 
+		print_r($_SESSION);
+		?>
 	</body>
 </html>
