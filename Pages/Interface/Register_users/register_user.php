11dc5ba2969fe29b25cc72e2954acb5ff532b131
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-01-12 09:22:22 +0000

    Update register_user.php

--- Pages/Interface/Register_users/register_user.php
+++ Pages/Interface/Register_users/register_user.php
@@ -4,6 +4,12 @@ include "/Includes/Php/check_user_pass.php";
 include "/Includes/Php/dbh.php";
 $sql = "SELECT isAdmin FROM users WHERE username='".$_SESSION["username"]."';";
 $result = mysqli_query($conn, $sql);
+if (result == "yes") {
+    $is_admin = (true);
+}
+else {
+    header(location:"/Pages/Interface/No_perms/not_admin.php");
+}
 ?>
 <!DOCTYPE html>
 <html>
