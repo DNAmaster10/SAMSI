b8c194924dec5e6e4b2b39b2a2e3db804d1f89cf
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-01-12 09:28:04 +0000

    Update not_admin.php

--- Pages/Interface/No_perms/not_admin.php
+++ Pages/Interface/No_perms/not_admin.php
@@ -1,3 +1,16 @@
 <?php
 session_start();
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>SAMSi</title>
+  </head>
+  <body>
+    <p>Sorry <?php echo ($_SESSION["username"]); ?>! You must hold administrator priveliges to perform that action. If you feel this is an error, please contact a network administrator to help you with your problem.</p>
+    <br>
+    <form action="/Pages/Interface/main_menu.php">
+      <input type="submit" value="Home">
+    </form>
+  </body>
+</html>
