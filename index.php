075aea8c839067cb1a713bb2e63a774dba76426a
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-01-12 10:25:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,18 @@
 		<link rel="stylesheet" href="/Includes/Css/main.css">
 		<title>Samsi</title>
 	</head>
-    <body>
-        <p>Welcome to Samsi</p>
-        <form action="/Pages/Login/login.php">
-            <input type="button" onclick="location.href='/Pages/Login/login.php';" value="Login" />
-        </form>
-        <form action="/Pages/register.php">
-            <input type="button" onclick="location.href='/Pages/Register/register.php';" value="Register an admin account" />
-        </form>
-        <div style="position: relative; width: 600px; height: 800px;"> 
-	<br>
-<div class="footer">
-  <p>Pre-alpha v 0.0.2 - All rights reserved  THE SAMSI CORPORATION</p>
-</div>
-         <div style="position: absolute; bottom: 5px;">
-            Pre-alpha v 0.0.2 - All rights reserved  THE SAMSI CORPORATION
-         </div>
-        </div>
+    	<body>
+        	<p>Welcome to Samsi</p>
+        	<form action="/Pages/Login/login.php">
+           		<input type="button" onclick="location.href='/Pages/Login/login.php';" value="Login" />
+        	</form>
+        	<form action="/Pages/register.php">
+            		<input type="button" onclick="location.href='/Pages/Register/register.php';" value="Register an admin account" />
+        	</form>
+		<br>
+		<div class="footer">
+  			<p>Pre-alpha v 0.0.2 - All rights reserved  THE SAMSI CORPORATION</p>
+		</div>
 
     </body>
 </html>
