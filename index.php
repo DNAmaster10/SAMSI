b33235f8e4a4685d6439555eb380f4067bfe5d6a
theonewhocanbarelyprogram <94646797+theonewhocanbarelyprogram@users.noreply.github.com>
2022-01-12 10:24:09 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,11 @@
         <form action="/Pages/register.php">
             <input type="button" onclick="location.href='/Pages/Register/register.php';" value="Register an admin account" />
         </form>
-        <div style="position: relative; width: 600px; height: 800px;">    
-
+        <div style="position: relative; width: 600px; height: 800px;"> 
+	<br>
+<div class="footer">
+  <p>Pre-alpha v 0.0.2 - All rights reserved  THE SAMSI CORPORATION</p>
+</div>
          <div style="position: absolute; bottom: 5px;">
             Pre-alpha v 0.0.2 - All rights reserved  THE SAMSI CORPORATION
          </div>
