8d236b1fdb4df38a1b4fe1e1607b474690e274f3
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-01-31 10:36:52 +0000

    Update register_users_submit.php

--- Pages/Interface/Register_users/register_users_submit.php
+++ Pages/Interface/Register_users/register_users_submit.php
@@ -1,2 +1,15 @@
 <?php
 ?>
+<html>
+  <head>
+    <title>SAMSi</title>
+  </head>
+  <body>
+    <p><?php echo $text_output ?></p>
+    <br>
+    <form>
+      <input type="submit" value="Register someone else">
+    </form>
+    <br>
+  </body>
+</html>
