ed1f929e079a2099433d4017639a0da38ba5a67a
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-01-31 14:49:57 +0000

    Update register_users_submit.php

--- Pages/Interface/Register_users/register_users_submit.php
+++ Pages/Interface/Register_users/register_users_submit.php
@@ -1,8 +1,14 @@
 <?php
+//Connect to database
 include "/var/wwww/html/Includes/dbh.php";
+//Make sure user is logged in
 include "/var/www/html/Includes/check_user_pass.php";
+//get variables from HTML form
 $username = $_POST["username"];
 $password = $_POST["password"];
+$account_type = $_POST["account_type"];
+
+if ($account_type != 
 ?>
 <html>
   <head>
