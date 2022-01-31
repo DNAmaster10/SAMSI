b054d75538096842481b56c205f9bbc2db7892b9
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-01-31 15:37:01 +0000

    Update register_users_submit.php

--- Pages/Interface/Register_users/register_users_submit.php
+++ Pages/Interface/Register_users/register_users_submit.php
@@ -1,14 +1,35 @@
 <?php
 //Connect to database
 include "/var/wwww/html/Includes/dbh.php";
+
 //Make sure user is logged in
 include "/var/www/html/Includes/check_user_pass.php";
+
 //get variables from HTML form
 $username = $_POST["username"];
 $password = $_POST["password"];
 $account_type = $_POST["account_type"];
 
-if ($account_type != 
+$continue = 1;
+
+//verify user input is correct
+if ($account_type !== "student") {
+  if ($account_type !== "teacher") {
+    if ($account_type !== "admin") {
+      $text_output = "There was an error selecting the account type!";
+      $continue = 0;
+    }
+  }
+}
+
+if ($continue == 1) {
+//writing to DB
+$table_name = "users";
+$columns = ("username,password,account_type");
+$values = ("'.$username.','.$password.','.$account_type.'");
+include "/var/www/html/Includes/write_to_db.php";
+$text_output = ("Account ".$username." was successfully registered");
+}
 ?>
 <html>
   <head>
@@ -17,8 +38,11 @@ if ($account_type !=
   <body>
     <p><?php echo $text_output ?></p>
     <br>
-    <form>
+    <form action="./register_user.php">
       <input type="submit" value="Register someone else">
+    </form>
+    <form action="././main_menu.php">
+      <input type="submit" value="Home">
     </form>
     <br>
   </body>
