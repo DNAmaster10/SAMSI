f87227321a64d13ba8108f87f2696cb8faf1347b
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-01 11:29:34 +0000

    Update register.php

--- Pages/Register/register.php
+++ Pages/Register/register.php
@@ -7,8 +7,8 @@
   <body>
     <form action="/Pages/Register/schoolRegComplete.php" method="POST">
       <p>School Name:</p> <input type="text" name="schoolName"><br>
-      <p>Admin Account Name:</p> <input type="text" name="adminName"><br>
-      <p>Admin Account Password:</p> <input type="text" name="adminPassword">
+      <p>Admin Account Name:</p> <input type="text" name="adminName" required><br>
+      <p>Admin Account Password:</p> <input type="text" name="adminPassword" required>
       <input type="submit" value="register">
     </form>
   </body>
