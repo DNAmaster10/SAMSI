08b0cd88b699edc370cfd58f3a4e471de4afbb5a
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-02 12:23:21 +0000

    Update get_single_value_from_db.php

--- Includes/Php/get_single_value_from_db.php
+++ Includes/Php/get_single_value_from_db.php
@@ -1,7 +1,7 @@
 <?php
 #Variables needed "$table_name", "$column_name", "$where_column" and "$where_value"
 #Outputs value as "$result"
-$sql = "SELECT ".$column_name." FROM ".$table_name." WHERE (".$where_column.")=('".$where_value."');";
+$sql = "SELECT ".$column_name." FROM ".$table_name." WHERE (".$where_column.") = ('".$where_value."');";
 $raw_result = mysqli_query($conn, $sql) or die (mysqli_error($conn));
 if ($raw_result->num_rows > 0) {
 	$row = $raw_result->fetch_assoc();
