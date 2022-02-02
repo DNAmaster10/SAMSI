328099d5245b006fba41010f52289ed40e5a216d
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-02 11:35:09 +0000

    Update register_users_submit.php

--- Pages/Interface/Register_users/register_users_submit.php
+++ Pages/Interface/Register_users/register_users_submit.php
@@ -12,8 +12,19 @@ include "/var/www/html/Includes/Php/check_user_pass.php";
 $username = $_POST["username"];
 $password = $_POST["password"];
 $account_type = $_POST["account_type"];
-
 $continue = 1;
+
+//check if the username already exists in database
+$table_name = "users";
+$column_name = "account_type";
+$where_column = "username";
+$where_value = "'".$username."'";
+include "/var/www/html/Includes/Php/get_single_value_from_db.php";
+
+if ($result !== "null") {
+$continue = 0;
+$text_output = "An account with that username has already been registered!";
+}
 
 if ($continue == 1) {
 //writing to DB
@@ -22,6 +33,8 @@ $columns = "username,password,account_type";
 $values = "'".$username."','".$password."','".$account_type."'";
 include "/var/www/html/Includes/Php/write_to_db.php";
 $text_output = ("Account ".$username." was successfully registered");
+
+
 }
 ?>
 <html>
