3428ff1d36e5fe13d2b661784712bbf1bbc69215
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-04 11:42:25 +0000

    Create theme_submit.php

---
+++ Pages/Interface/Theme/theme_submit.php
@@ -0,0 +1,14 @@
+<?php
+include "/var/www/html/Includes/Php/dbh.php";
+include "/var/www/html/Includes/Php/check_user_pass.php";
+include "/var/www/html/Includes/Php/get_user_theme.php";
+?>
+<html>
+  <head>
+    <title>SAMSi</title>
+    <link rel="stylesheet" href="/Includes/Css/Themes/<?php echo $theme; ?>.css">
+    <link rel="stylesheet" href="/Includes/Css/main.css">
+  </head>
+  <body>
+  </body>
+</html>
