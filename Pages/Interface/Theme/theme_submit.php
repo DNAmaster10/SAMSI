e31f94840fc2d92bd2cb2850c1de5ab0774264e3
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-04 14:43:10 +0000

    Update theme_submit.php

--- Pages/Interface/Theme/theme_submit.php
+++ Pages/Interface/Theme/theme_submit.php
@@ -3,8 +3,9 @@ include "/var/www/html/Includes/Php/dbh.php";
 include "/var/www/html/Includes/Php/check_user_pass.php";
 include "/var/www/html/Includes/Php/get_user_theme.php";
 
-
-
+$selected_theme = $_POST["theme_select"];
+$sql = "UPDATE themes SET theme = '".$selected_theme."' WHERE username='".$_SESSION["username"]."';";
+mysqli_query($conn, $sql) or die (mysqli_error($conn));
 header('Location: select_theme.php');
 ?>
 <html>
