84e840b4ecfa5c6f0fbf7ea5ef8da5f97be3bba0
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-04 09:34:18 +0000

    Update get_user_theme.php

--- Includes/Php/get_user_theme.php
+++ Includes/Php/get_user_theme.php
@@ -3,7 +3,7 @@ $sql = "SELECT (theme) FROM (themes) WHERE (username)=('".$username."');";
 $raw_result = mysqli_query($conn, $sql) or die (mysqli_error($conn));
 if ($raw_result->num_rows > 0) {
 	$row = $raw_result->fetch_assoc();
-	$result = $row[$column_name];
+	$result = $row["theme"];
 	unset($row);
 	unset($raw_result);
 }
