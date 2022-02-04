256394730aea410b0fa6049864915d1a0e7cc9ae
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-04 11:42:21 +0000

    Update select_theme.php

--- Pages/Interface/Theme/select_theme.php
+++ Pages/Interface/Theme/select_theme.php
@@ -10,6 +10,10 @@ include "/var/www/html/Includes/Php/get_user_theme.php";
 		<link rel="stylesheet" href="/Includes/Css/Themes/<?php echo $theme ?>.css">
 	</head>
 	<body>
-	<p>Use this page to select your desired themes</p>
+		<p>Use this page to select your desired themes</p>
+		<br>
+		<form action="theme_submit.php">
+			
+		</form>
 	</body>
 </html>
