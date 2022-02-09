adb24dbe9437b46cd65921d3f04011972077e444
theonewhocanbarelyprogram <94646797+theonewhocanbarelyprogram@users.noreply.github.com>
2022-02-09 16:18:30 +0000

    Update login.php

--- Pages/Login/login.php
+++ Pages/Login/login.php
@@ -22,7 +22,7 @@
   		margin-left: 750px;	    
            	width: 20%;
 
-      background-color: #ffffff;
+      background-color: #000000;
       text-align: center;
 	  border-radius: 25px;
 	  height: 50%;
