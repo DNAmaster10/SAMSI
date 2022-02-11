dc80205002aca801021b4b4ffabf886f982be427
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-11 14:44:46 +0000

    Update get_account_type.php

--- Includes/Php/get_account_type.php
+++ Includes/Php/get_account_type.php
@@ -4,7 +4,7 @@ $sql = "SELECT account_type FROM users WHERE username = ('".$username."');";
 $raw_result = mysqli_query($conn, $sql) or die (mysqli_error($conn));
 if ($raw_result->num_rows > 0) {
 	$row = $raw_result->fetch_assoc();
-	$account_type = $row[$column_name];
+	$account_type = $row["account_type"];
 	unset($row);
 	unset($raw_result);
 }
