252fd5af6f5de3e70e718c812dcadc38a27136d3
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-28 10:15:11 +0000

    Update delete_accounts_submit.php

--- Pages/Interface/Manage_accounts/Delete_accounts/delete_accounts_submit.php
+++ Pages/Interface/Manage_accounts/Delete_accounts/delete_accounts_submit.php
@@ -1,1 +1,12 @@
 <?php
+$file_path = $_SERVER["DOCUMENT_ROOT"];
+session_start();
+include $file_path."/Includes/Php/dbh.php";
+include $file_path."/Includes/Php/check_user_pass.php";
+include $file_path."/Includes/Php/get_account_type";
+if ($account_type == "admin") {
+  include $file_path."/Includes/Php/get_user_theme";
+}
+else {
+	header("location: /Pages/Interface/No_perms/not_admin.php");
+}
