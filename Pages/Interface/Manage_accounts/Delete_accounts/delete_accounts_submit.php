0e24aabaf7b9670576a507c672fc6cbafcc086c0
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-02-28 10:24:25 +0000

    Update delete_accounts_submit.php

--- Pages/Interface/Manage_accounts/Delete_accounts/delete_accounts_submit.php
+++ Pages/Interface/Manage_accounts/Delete_accounts/delete_accounts_submit.php
@@ -1,4 +1,5 @@
 <?php
+$deletion_username = $_POST["deletion_username"];
 $file_path = $_SERVER["DOCUMENT_ROOT"];
 session_start();
 include $file_path."/Includes/Php/dbh.php";
@@ -6,7 +7,10 @@ include $file_path."/Includes/Php/check_user_pass.php";
 include $file_path."/Includes/Php/get_account_type";
 if ($account_type == "admin") {
   include $file_path."/Includes/Php/get_user_theme";
+  $sql = "DELETE FROM users WHERE username='".$deletion_username."';
+  DELETE FROM user_classes WHERE username='".$deletion_username."';
+  DELETE FROM themes WHERE username='".$deletion_username."'";
 }
 else {
-	header("location: /Pages/Interface/No_perms/not_admin.php");
+  header("location: /Pages/Interface/No_perms/not_admin.php");
 }
