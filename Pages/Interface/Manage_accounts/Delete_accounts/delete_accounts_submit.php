f578914cc83054014c67169a09680f794d37fe93
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-01 12:44:50 +0000

    Fixed bug (hopefully) with sql

--- Pages/Interface/Manage_accounts/Delete_accounts/delete_accounts_submit.php
+++ Pages/Interface/Manage_accounts/Delete_accounts/delete_accounts_submit.php
@@ -7,8 +7,12 @@ include $file_path."/Includes/Php/check_user_pass.php";
 include $file_path."/Includes/Php/get_account_type.php";
 if ($account_type == "admin") {
   include $file_path."/Includes/Php/get_user_theme.php";
-  $sql = "DELETE FROM users WHERE username='".$deletion_username."'; DELETE FROM user_classes WHERE username='".$deletion_username."'; DELETE FROM themes WHERE username='".$deletion_username."'";
-  mysqli_query($conn, $sql) or die (mysqli_error($conn));;
+  $sql = "DELETE FROM users WHERE username='".$deletion_username."';";
+  $sql2 = "DELETE FROM user_classes WHERE username='".$deletion_username."';";
+  $sql3 = "DELETE FROM themes WHERE username='".$deletion_username."';";
+  mysqli_query($conn, $sql) or die (mysqli_error($conn));
+  mysqli_query($conn, $sql2);
+  mysqli_query($conn, $sql3);
 }
 else {
   header("location: /Pages/Interface/No_perms/not_admin.php");
