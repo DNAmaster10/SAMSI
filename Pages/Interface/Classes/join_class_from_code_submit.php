470e397620b2fc241b5ac8328c5ccc7a1c14f240
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-02 15:05:32 +0000

    Update

--- Pages/Interface/Classes/join_class_from_code_submit.php
+++ Pages/Interface/Classes/join_class_from_code_submit.php
@@ -5,28 +5,41 @@ $file_path = $_SERVER["DOCUMENT_ROOT"];
 include $file_path."/Includes/Php/dbh.php";
 include $file_path."/Includes/Php/check_user_pass.php";
 include $file_path."/Includes/Php/get_user_theme.php";
-
 $table_name = "class_data";
 $column_name = "class";
 $where_column = "join_code";
 $where_value = $input_code;
 include $file_path."/Includes/Php/get_single_value_from_db.php";
 $class_name = $result;
-
 $table_name = "user_classes";
 $column_name = "classes";
 $where_column = "username";
 $where_value = $_SESSION["username"];
 include $file_path."/Includes/Php/get_single_value_from_db.php";
 $current_user_classes = $result;
-
 $current_classes_array = explode(',', $current_user_classes);
 if (in_array($class_name, $current_classes_array)){
 	$text_output = "You are already a member of that class!";
 }
 else {
 	$new_user_classes = $current_user_classes.$class_name;
-	$sql = "UPDATE user_classes SET classes='".$new_user_classes."' WHERE username='".$_SESSION['username']."';";
+	$sql = "UPDATE user_classes SET classes='".$new_user_classes.",' WHERE username='".$_SESSION['username']."';";
 	mysqli_query ($conn,$sql);
+	$text_output = "Done! You're now a member of ".$class_name;
 }
 >
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SAMSi</title>
+	</head>
+	<body>
+		<p><?php echo $text_output ?></p>
+		<form action="./class_select_menu.php">
+			<input type="submit" value="Classes">
+		</form>
+		<form action="../main_menu.php">
+			<input type="submit" value="Home">
+		</form>
+	</body>
+</html>
