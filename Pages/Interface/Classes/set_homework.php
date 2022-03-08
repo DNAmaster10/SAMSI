13184e42e96b2357e2e399b664df5bfac5529607
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-08 11:34:08 +0000

    update

---
+++ Pages/Interface/Classes/set_homework.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    $file_path = $_SERVER["DOCUMENT_ROOT"];
+    $username = $_SESSION["username"];
+    include $file_path."/Includes/Php/dbh.php";
+    include $file_path."/Includes/Php/check_user_pass.php";
+    include $file_path."/Includes/Php/get_account_type.php";
+    include $file_path."/Includes/Php/get_user_theme.php";
+    
+    if ((!isset($_SESSION["current_class"])) {
+        header ("location: /Pages/Interface/Misc/class_not_selected.php");
+    }
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SAMSi</title>
+        <link rel="stylesheet" href="/Includes/Css/Themes/<?php echo $theme ?>.css">
+        <link rel="stylesheet" href="/Includes/Css/main.css">
+    </head>
+</html>
