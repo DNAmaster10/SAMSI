e50ec740d43cac3d19b98d4daaa95655f03f0ae9
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-10 15:08:43 +0000

    update

--- Pages/Interface/Homework/view_homework.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	$file_path = $_SERVER["DOCUMENT_ROOT"];
-	$username = $_SESSION["username"];
-	include "/var/www/html/Includes/Php/dbh.php";
-	include "/var/www/html/Includes/Php/check_user_pass.php";
-	include "/var/www/html/Includes/Php/get_user_theme.php";
-	include "/var/www/html/Includes/Php/get_account_type.php";
-?>
