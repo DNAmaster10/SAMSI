79b241db4ac15614b8ab26afc489a9bd290e697a
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-16 16:25:49 +0000

    update

--- Pages/Interface/Classes/set_homework_submit.php
+++ Pages/Interface/Classes/set_homework_submit.php
@@ -77,7 +77,7 @@
 	else {
         $members_array = explode(',', $members);
         $student_number = 0;
-        $sql = "UPDATE SET not_complete='".$members."' WHERE ID='".$ident."';";
+        $sql = "UPDATE homework_data SET not_complete='$members' WHERE ID='$ident';";
         mysqli_query($conn,$sql) or die (mysqli_error($conn));
         for ($i = 0; $i <= $member_ammount; $i++) {
             #Add the homework title
