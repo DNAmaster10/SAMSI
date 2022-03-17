7c47ac1665dde8fda2549a16f7ec0e06d1b12036
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-17 16:31:43 +0000

    update

--- Pages/Interface/Classes/Javascript/set_homework.js
+++ Pages/Interface/Classes/Javascript/set_homework.js
@@ -1,1 +1,4 @@
-document.getElementById("submit_date").value = new Date(year,month,day);
+var year = getFullYear();
+var month = getMonth();
+var day = getDay();
+document.getElementById("submit_date").value = new Date(day,month,year);
