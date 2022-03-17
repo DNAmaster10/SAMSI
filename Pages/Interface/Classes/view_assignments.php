07a03727d47e5262f28f57a99123952d9cc9b6d2
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-17 12:39:25 +0000

    upddate

--- Pages/Interface/Classes/view_assignments.php
+++ Pages/Interface/Classes/view_assignments.php
@@ -7,12 +7,34 @@
     include $file_path."/Includes/Php/get_account_type.php";
     include $file_path."/Includes/Php/get_user_theme.php";
 	
+	#Check that user is owner of the class
+	$table_name = "class_data";
+	$column_name = "owner";
+	$where_column = "class";
+	$where_value = $_SESSION["current_class"];
+	include $file_path."/Includes/Php/get_single_value_from_db.php";
+	if ($result == $_SESSION["username"]; {
+        $is_owner = 1;
+	}
+	else {
+        $is_owner = 0;
+	}
+	
 	#getting class assignments
 	$table_name = "class_data";
 	$column_name = "homework";
 	$where_column = "class";
 	$where_value = $_SESSION["current_class"];
 	include $file_path."/Includes/Php/get_single_value_from_db.php";
+	$homework_id_list = $result;
+	if ($homework_id_list == "null") {
+        $is_homework = 0;
+	}
+	else {
+        $is_homework = 1;
+        $homework_count = substr_count($homework_id_list,",");
+        $homework_id_array = explode(",",$homework_id_list);
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,6 +44,58 @@
 		<link rel="stylesheet" href="/Includes/Css/main.css">
 	</head>
 	<body>
-		<h1>Current assignments<h1>
+		<h1>Current assignments:</h1>
+		<?php
+		if ($is_homework == 1) {
+				echo "<p>You're all up to date!</p>";
+			}
+			else {
+            for ($i = 0; $i <= $homework_count - 1; $i++) {
+            $table_name = "homework_data";
+            $column_name = "title";
+            $where_column = "ID";
+            $where_value = $homework_id_array[$i];
+            include $file_path."/Includes/Php/get_single_value_from_db.php";
+            $output_title = $result;
+            
+            $table_name = "homework_data";
+            $column_name = "description";
+            $where_column = "ID";
+            $where_value = $homework_id_array[$i];
+            include $file_path."/Includes/Php/get_single_value_from_db.php";
+            $output_description = $result;
+            
+            $table_name = "homework_data";
+            $column_name = "due_date";
+            $where_column = "ID";
+            $where_value = $homework_id_array[$i];
+            include $file_path."/Includes/Php/get_single_value_from_db.php";
+            $output_due_date = $result;
+            
+            $table_name = "homework_data";
+            $column_name = "date_set";
+            $where_column = "ID";
+            $where_value = $homework_id_array[$i];
+            include $file_path."/Includes/Php/get_single_value_from_db.php";
+            $output_date_set = $result;
+            
+            echo "
+			<div class='homework_div'>
+				<h3> ".$output_title."</h3>
+				<p> Description: ".$output_description."</p>
+				<p><small> Date due: ".$output_due_date."| Date set: ".$output_date_set."|</small></p>
+				"
+				if ($is_owner == 1) {echo "
+				<form action='./edit_assignment.php' method='POST'>
+					<p> </p><input type='hidden' name='homework_id' value='".($homework_id_array[$i])."'>
+					<input type='submit' value='Edit details'>
+				</form>
+				"; }
+				echo "
+				<p> </p>
+            </div>"; 
+            }
+			}
+			?>
 	</body>
 </html>
