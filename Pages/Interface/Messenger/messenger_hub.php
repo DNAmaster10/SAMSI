0398127f87fa0a20f24202942bdab06598a4a588
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-22 18:06:18 +0000

    u

--- Pages/Interface/Messenger/messenger_hub.php
+++ Pages/Interface/Messenger/messenger_hub.php
@@ -6,7 +6,6 @@
     include $file_path."/Includes/Php/check_user_pass.php";
     include $file_path."/Includes/Php/get_account_type.php";
     include $file_path."/Includes/Php/get_user_theme.php";
-    
 ?>
 <!DOCTYPE html>
 <html>
