4414cb82391638c69a478bf9f90d252aa7ee5fa0
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-22 11:46:45 +0000

    update

---
+++ Pages/Interface/Messenger/messenger_hub.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+    $file_path = $_SERVER["DOCUMENT_ROOT"];
+    $username = $_SESSION["username"];
+    include $file_path."/Includes/Php/dbh.php";
+    include $file_path."/Includes/Php/check_user_pass.php";
+    include $file_path."/Includes/Php/get_account_type.php";
+    include $file_path."/Includes/Php/get_user_theme.php";
+?>
