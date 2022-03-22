d0494decc9bd7ec113c9afd3bed8de3dae9d7fb0
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-22 16:39:18 +0000

    update

---
+++ Pages/Interface/Messenger/create_chat.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    $file_path = $_SERVER["DOCUMENT_ROOT"];
+    $username = $_SESSION["username"];
+    include $file_path."/Includes/Php/dbh.php";
+    include $file_path."/Includes/Php/check_user_pass.php";
+    include $file_path."/Includes/Php/get_account_type.php";
+    include $file_path."/Includes/Php/get_user_theme.php";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SAMSi<title>
+		<link rel="stylesheet" href="/Includes/Css/Themes/<?php echo $theme ?>.css">
+		<link rel="stylesheet" href="/Includes/Css/main.css">
+    </head>
+    <body>
+        <form action="./create_chat_submit.php" method="POST">
+            <p> Chat name: </p><input type="text" name="chat__name" required>
+            <p> Other member(s): </p><input type="text" name="members" placeholder="James,ruby02,max">
+            <input type="submit" value="Create">
+        </form>
+    </body>
+</html>
