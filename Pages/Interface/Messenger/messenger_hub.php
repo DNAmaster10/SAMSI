020339a4151a2e1b45f54acc9159058e5ee1a243
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-22 16:24:52 +0000

    update

--- Pages/Interface/Messenger/messenger_hub.php
+++ Pages/Interface/Messenger/messenger_hub.php
@@ -6,4 +6,26 @@
     include $file_path."/Includes/Php/check_user_pass.php";
     include $file_path."/Includes/Php/get_account_type.php";
     include $file_path."/Includes/Php/get_user_theme.php";
+    
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SAMSi<title>
+		<link rel="stylesheet" href="/Includes/Css/Themes/<?php echo $theme ?>.css">
+		<link rel="stylesheet" href="/Includes/Css/main.css">
+    </head>
+    <body>
+        <form action="/Pages/Interface/main_menu.php">
+            <input type="submit" value="Home">
+        </form>
+        <form action="./join_chat.php">
+            <input type="submit" value="Join chat from code">
+        </form>
+        <form action-"./create_chat.php">
+            <input type="submit" value="Create chat">
+        </form>
+        <br>
+        
+    </body>
+</html>
