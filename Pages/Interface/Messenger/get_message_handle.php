3d552e844d9f2fc80aeb870fca72743032722ea8
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-24 16:22:13 +0000

    update

---
+++ Pages/Interface/Messenger/get_message_handle.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    $file_path = $_SERVER["DOCUMENT_ROOT"];
+    include $file_path."/Includes/Php/dbh.php";
+    $chat_id = $_SESSION["current_chat"];
+    $table_name = "chat_data";
+    $column_name = "users";
+    $where_column = "chat_id";
+    $where_value = $chat_id;
+    include $file_path."/Includes/Php/get_single_value_from_db.php";
+    $members_array = explode (",", $result);
+    if (!in_array($_SESSION["username"], $members_array)) {
+        echo "You are not a part of this chat!";
+    }
+    else {
+        $sql = "SELECT message_id FROM message_data WHERE chat_id='$chat_id' ORDER BY message_id DESC LIMIT 10";
+    }
+?>
