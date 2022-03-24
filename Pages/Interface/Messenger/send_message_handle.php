a8b3c3f68003a536fdfb6db0aac981e1aa4f1e1c
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-24 15:28:58 +0000

    update

--- Pages/Interface/Messenger/send_message_handle.php
+++ Pages/Interface/Messenger/send_message_handle.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     $file_path = $_SERVER["DOCUMENT_ROOT"];
-    include $file_path."/Includes/Php/get_account_type.php"
+    include $file_path."/Includes/Php/get_account_type.php";
     $table_name = "chat_data";
     $column_name = "users";
     $where_column = "chat_id";
