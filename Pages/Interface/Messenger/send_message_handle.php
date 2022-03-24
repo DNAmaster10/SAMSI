a5fe544ca214a34269e2380241d0d4611f253254
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-24 15:06:57 +0000

    update

--- Pages/Interface/Messenger/send_message_handle.php
+++ Pages/Interface/Messenger/send_message_handle.php
@@ -1,5 +1,5 @@
 <?php
-    user_input = $_POST["message"];
-    user_input = user_input * 2;
-    echo user_input;
+    $user_input = $_POST["message"];
+    $user_input = $user_input * 2;
+    echo $user_input;
 ?>
