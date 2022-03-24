5e9548c8df019ea3e6a9119c40710fb8ff5d1380
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-03-24 16:48:55 +0000

    update

--- Pages/Interface/Messenger/get_message_handle.php
+++ Pages/Interface/Messenger/get_message_handle.php
@@ -16,7 +16,11 @@
         $sql = "SELECT message_id FROM message_data WHERE chat_id='$chat_id' ORDER BY message_id DESC LIMIT 10";
         $raw_result = mysqli_query ($conn, $sql) or die (mysqli_error($conn));
         if ($raw_result -> num_rows > 0) {
-            $message_id_array = $raw_result -> fetch_array(MYSQLI_NUM);
+            $message_id_array = array();
+            while ($row = mysqli_fetch_assoc($raw_result)) {
+                $message_id_array[] = mysqli_result($row, 0);
+            }
+            $message_id_array 
             $message_id_string = implode (",",$message_id_array);
             echo $message_id_string;
         }
