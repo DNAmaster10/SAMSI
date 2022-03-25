511dd1e90f22b7b8eeb3f7906588296cd195909b
DNAmaster10 <louishollobon@outlook.com>
2022-03-25 22:22:26 +0000

    Update select_theme.php

--- Pages/Interface/Theme/select_theme.php
+++ Pages/Interface/Theme/select_theme.php
@@ -16,15 +16,15 @@ include "/var/www/html/Includes/Php/get_user_theme.php";
 		<p>Use this page to select your desired themes</p>
 		<br>
 		<form action="theme_submit.php" method="POST">
-			<input class="coloured_text" type="radio" value="default" name="theme_select" <?php if ($theme == "default") {echo "checked";} ?>><label>Default</label><br>
-			<input class="coloured_text" type="radio" value="dark" name="theme_select" <?php if ($theme == "dark") {echo "checked";} ?>><label>Dark Mode</label><br>
-			<input class="coloured_text" type="radio" value="cleo" name="theme_select" <?php if ($theme == "cleo") {echo "checked";} ?>><label>Cleo</label><br>
-			<input class="coloured_text" type="radio" value="darkcleo" name="theme_select" <?php if ($theme == "darkcleo") {echo "checked";} ?>><label>Dark Cleo</label><br>
-			<input class="coloured_text" type="radio" value="rose" name="theme_select" <?php if ($theme == "rose") {echo "checked";} ?>><label>Rose</label><br>
-			<input class="coloured_text" type="radio" value="darkrose" name="theme_select" <?php if ($theme == "darkrose") {echo "checked";} ?>><label>Dark Rose</label><br>
-			<input class="coloured_text" type="radio" value="lavender" name="theme_select" <?php if ($theme == "lavender") {echo "checked";} ?>><label>Lavender</label><br>
-			<input class="coloured_text" type="radio" value="darklavender" name="theme_select" <?php if ($theme == "darklavender") {echo "checked";} ?>><label>Dark Lavender</label><br>
-			<input class="coloured_text" type="radio" value="peach" name="theme_select" <?php if ($theme == "peach") {echo "checked";} ?>><label>Peach</label>
+			<input class="coloured_text" type="radio" value="default" name="theme_select" <?php if ($theme == "default") {echo "checked";} ?>><label>Default</label class="coloured_text"><br>
+			<input class="coloured_text" type="radio" value="dark" name="theme_select" <?php if ($theme == "dark") {echo "checked";} ?>><label>Dark Mode</label class="coloured_text"><br>
+			<input class="coloured_text" type="radio" value="cleo" name="theme_select" <?php if ($theme == "cleo") {echo "checked";} ?>><label>Cleo</label class="coloured_text"><br>
+			<input class="coloured_text" type="radio" value="darkcleo" name="theme_select" <?php if ($theme == "darkcleo") {echo "checked";} ?>><label class="coloured_text">Dark Cleo</label><br>
+			<input class="coloured_text" type="radio" value="rose" name="theme_select" <?php if ($theme == "rose") {echo "checked";} ?>><label>Rose</label class="coloured_text"><br>
+			<input class="coloured_text" type="radio" value="darkrose" name="theme_select" <?php if ($theme == "darkrose") {echo "checked";} ?>><label class="coloured_text">Dark Rose</label><br>
+			<input class="coloured_text" type="radio" value="lavender" name="theme_select" <?php if ($theme == "lavender") {echo "checked";} ?>><label class="coloured_text">Lavender</label><br>
+			<input class="coloured_text" type="radio" value="darklavender" name="theme_select" <?php if ($theme == "darklavender") {echo "checked";} ?>><label class="coloured_text">Dark Lavender</label><br>
+			<input class="coloured_text" type="radio" value="peach" name="theme_select" <?php if ($theme == "peach") {echo "checked";} ?>><label class="coloured_text">Peach</label>
 			<br>
 			<input type="submit" value="Save" class="block_button">
 		</form>
