1551552e1a6710764d77a996c11c817e417c3109
DNAmaster10 <louishollobon@outlook.com>
2022-03-25 19:48:31 +0000

    Update class_select_menu.php

--- Pages/Interface/Classes/class_select_menu.php
+++ Pages/Interface/Classes/class_select_menu.php
@@ -43,7 +43,7 @@
 	<br>
 		<p>Not a member of any classes? Enter the join code here!</p>
 		<form action="./join_class_from_code_submit.php">
-			<input type="text" name="join_code" placeholder="1234">
+			<input type="text" name="input_code" placeholder="1234" required>
 			<input type="submit" value="Join">
 		</form>
 	<br>
