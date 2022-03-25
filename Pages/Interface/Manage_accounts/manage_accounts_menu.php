626056dc4406935615ddec342a3fe3b80b02fde6
DNAmaster10 <louishollobon@outlook.com>
2022-03-25 20:00:28 +0000

    Update manage_accounts_menu.php

--- Pages/Interface/Manage_accounts/manage_accounts_menu.php
+++ Pages/Interface/Manage_accounts/manage_accounts_menu.php
@@ -25,7 +25,7 @@ else {
   </head>
   <body>
     <h1>Where would you like to go?</h1><br>
-	<form action="./main_menu.php">
+	<form action="../main_menu.php">
 		<input type="submit" value="Back">
 	</form>
     <form action="./Register_users/register_user.php">
