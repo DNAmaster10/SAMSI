dcf852cd258c0baf13a3e17daf1eb9a5e233acc0
DNAmaster10 <louishollobon@outlook.com>
2022-03-25 23:01:09 +0000

    e

--- Pages/Interface/Manage_accounts/Delete_accounts/delete_accounts.php
+++ Pages/Interface/Manage_accounts/Delete_accounts/delete_accounts.php
@@ -19,12 +19,30 @@ include $file_path."/Includes/Php/get_user_theme.php";
 		<link rel="stylesheet" href="/Includes/Css/Themes/<?php echo $theme; ?>.css">
 		<link rel="stylesheet" href="/Includes/Css/main.css">
 	</head>
+	<style>
+		.select_form{
+			display: inline-block;
+		}
+		.block_button{
+			padding: 15px 32px;
+			font-size: 16px;
+			display: inline-block;
+		}
+	</style>
 	<body>
 		<h1>Hey <?php echo $_SESSION["username"]; ?>! Use this page to remove users from the system.</h1>
+		<form action="../../main_menu.php" class="select_form">
+			<input type="submit" value="Home" class="block_button">
+		</form>
+		<form action="../manage_accounts_menu.php" class="select_form">
+			<input type="submit" value="Back" class="block_button">
+		</form>
 		<br>
 		<form action="./delete_accounts_submit.php" method="POST">
 			Username: <input type="text" placeholder="JohnJames123" name="deletion_username" required>
 			<input type="submit" value="Delete">
 		</form>
+		<input type="text" placeholder="username" onkeyup="getUsers()" id="username_input_box">
 	</body>
+	<script src="./Javascript/delete_accounts.js"></script>
 </html>
