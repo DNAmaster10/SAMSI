c35fa37bc13d1dedbef4c63fe49934e684d53700
DNAmaster10 <louishollobon@outlook.com>
2022-03-25 22:40:34 +0000

    Update register_user.php

--- Pages/Interface/Manage_accounts/Register_users/register_user.php
+++ Pages/Interface/Manage_accounts/Register_users/register_user.php
@@ -42,12 +42,14 @@ include "/var/www/html/Includes/Php/get_user_theme.php";
 	</style>
     <body>
         <h1>Register an account</h1>
+	<div class="select_form">
     <form action="../manage_accounts_menu.php" class="select_form">
     <input type="submit" value="Back" class="block_button">
     </form><br>
 	<form action="../../main_menu.php" class="select_form">
 		<input type="submit" value="Home" class="block_button">
 	</form>
+	</div>
 	<p>Hey <?php echo ($_SESSION["username"]); ?>! Use this page to register users, such as teachers and students.</p><br>
 	<br>
 	<div class="input_menu">
