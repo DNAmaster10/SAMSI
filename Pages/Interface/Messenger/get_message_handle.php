60e614e46f03850006b161efc37d60c979a9dcf6
DNAmaster10 <louishollobon@outlook.com>
2022-03-27 16:20:32 +0100

    Update get_message_handle.php

--- Pages/Interface/Messenger/get_message_handle.php
+++ Pages/Interface/Messenger/get_message_handle.php
@@ -21,9 +21,11 @@
         if ($raw_result -> num_rows > 0) {
             while ($row = mysqli_fetch_array($raw_result)){
                 if ($row["user"] == $_SESSION[$username]) {
-                 echo "You: ".$row["message"]."<br />";
+                    echo "You: ".$row["message"]."<br />";
                 }
-				echo $row['user'].": ".$row["message"]."<br />";
+                else {
+                    echo $row['user'].": ".$row["message"]."<br />";
+                }
             }
         }
         else {
