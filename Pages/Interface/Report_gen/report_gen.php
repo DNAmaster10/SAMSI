fa6b39b3923f1e4a9307969e38b9b7996abeb51c
DNAmaster10 <louishollobon@outlook.com>
2022-03-27 18:49:13 +0100

    Update report_gen.php

--- Pages/Interface/Report_gen/report_gen.php
+++ Pages/Interface/Report_gen/report_gen.php
@@ -19,16 +19,18 @@
 		<link rel="stylesheet" href="/Includes/Css/main.css">
     </head>
     <body>
+        <div class="header">
         <form action="../main_menu.php">
             <input type="submit" value="Back" class="block_button">
         </form>
+        </div>
         <form action="./report_gen_submit.php" method="POST">
             <input type="text" placeholder="Name" name="name" required>
             <input type="text" placeholder="Subject" name="subject" required>
             <input type="text" placeholder="Grade" name="grade" required>
             <input type="submit" value="Generate Report" class="small_block_button">
         </form>
-        <textarea class="report_gen_output" readonly><?php if(isset($_SESSION["report"])) {
+        <textarea class="report_gen_output" class="large_text_box" readonly><?php if(isset($_SESSION["report"])) {
             echo $_SESSION["report"];
         } ?></textarea>
     </body>
