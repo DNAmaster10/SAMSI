452b53d7e6635ba5d44cafd062f91c2582224edf
DNAmaster10 <louishollobon@outlook.com>
2022-03-27 19:27:02 +0100

    Update logout.php

--- Pages/Logout/logout.php
+++ Pages/Logout/logout.php
@@ -6,12 +6,14 @@ session_unset();
 <html>
 	<head>
 		<title>SAMSi</title>
+		<link rel="stylesheet" href="/Includes/Css/Themes/default.css">
+		<link rel="stylesheet" href="/Includes/Css/main.css">
 	</head>
 	<body>
 		<h1>Later, <?php echo($logout_name); ?>
 		<br>
 		<form action="../../index.php">
-			<input type="submit" value="Home page">
+			<input type="submit" value="Home page" class="block_button">
 		</form>
 	</body>
 </html>
