10f63273c0f4e4224b0d5d8b9bb49c208d76b285
DNAmaster10 <louishollobon@outlook.com>
2022-03-27 12:49:28 +0100

    Update message_gui.js

--- Pages/Interface/Messenger/Javascript/message_gui.js
+++ Pages/Interface/Messenger/Javascript/message_gui.js
@@ -1,5 +1,5 @@
 function sendMessage() {
-    document.getElementById("send_message_container").class = "send_message_container_active";
+    document.getElementById("send_message_container").classList.toggle("send_message_container_active");
 
     input_message = document.getElementById("message_entry").value;
     document.getElementById("message_entry").value = "";
@@ -9,7 +9,7 @@ function sendMessage() {
         data: {message:input_message},
         success: function(data) {
             console.log(data);
-            document.getElementById("send_message_container").class = "send_message_container";
+            document.getElementById("send_message_container").classList.toggle("send_message_container");
         }
     });
 }
