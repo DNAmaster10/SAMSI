b7f5e97efbe03f2750aee76c005767e2093c37d3
DNAmaster10 <louishollobon@outlook.com>
2022-03-27 02:57:35 +0100

    update

--- Pages/Interface/Classes/edit_assignment.php
+++ Pages/Interface/Classes/edit_assignment.php
@@ -61,7 +61,7 @@
 		<p>Editing assignment: <?php echo "$title"; ?></p>
 		<form action="./edit_assignment_submit.php" method="post">
 			<p> Title:</p><input type="text" value="<?php echo $title; ?>" name="title" required>
-			<p> Description:</p><input type="text" rows="4" cols="50" name="description" value="<?php echo $description; ?>" required>
+			<p> Description:</p><input type="text" rows="4" cols="50" name="description" value="<?php echo $description; ?>" rows="4" cols="50" required>
 			<p> Due date:</p><input type="date" name="due_date" value="<?php echo $due_date; ?>" id="date" required>
 			<input type="hidden" value="<?php echo $homework_id; ?>" name="homework_id">
 			<input type="submit" value="Submit edits">
